efd5b52244a9cd8e4ee9f4a1182d0af87b53ab59
usuiat <stripewani@gmail.com>
2020-09-26 02:30:32 +0900

    Add NoteListActivity.

--- app/src/main/java/net/engawapg/app/camrepo/notelist/NoteListActivity.kt
+++ app/src/main/java/net/engawapg/app/camrepo/notelist/NoteListActivity.kt
@@ -1,11 +1,12 @@
-package net.engawapg.app.camrepo
+package net.engawapg.app.camrepo.notelist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.engawapg.app.camrepo.R
 
-class MainActivity : AppCompatActivity() {
+class NoteListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_note_list)
     }
 }
