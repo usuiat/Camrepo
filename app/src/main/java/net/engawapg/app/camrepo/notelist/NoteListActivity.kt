55c1412b2bcc0bce2c6947777ffa1e90cd24c2b9
usuiat <stripewani@gmail.com>
2020-09-26 03:34:17 +0900

    Add RecyclerView of CardView in NoteListActivity.

--- app/src/main/java/net/engawapg/app/camrepo/notelist/NoteListActivity.kt
+++ app/src/main/java/net/engawapg/app/camrepo/notelist/NoteListActivity.kt
@@ -2,11 +2,40 @@ package net.engawapg.app.camrepo.notelist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_note_list.*
 import net.engawapg.app.camrepo.R
 
 class NoteListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_list)
+
+        recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+            adapter = NoteCardAdapter()
+        }
     }
+
+    class NoteCardAdapter : RecyclerView.Adapter<NoteCardViewHolder>() {
+        override fun getItemCount(): Int {
+            return 10
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteCardViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val view = layoutInflater.inflate(R.layout.view_note_card, parent, false)
+            return NoteCardViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: NoteCardViewHolder, position: Int) {
+        }
+    }
+
+    class NoteCardViewHolder(v: View): RecyclerView.ViewHolder(v)
 }
