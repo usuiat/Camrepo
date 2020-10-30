b3c2c995450905ca98cd340fcde5ea61cf8c551c
usuiat <stripewani@gmail.com>
2020-10-30 13:24:02 +0900

    設定画面にツールバーと戻るボタンを追加。

--- app/src/main/java/net/engawapg/app/camrepo/settings/SettingsActivity.kt
+++ app/src/main/java/net/engawapg/app/camrepo/settings/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package net.engawapg.app.camrepo.settings
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+import kotlinx.android.synthetic.main.settings_activity.*
 import net.engawapg.app.camrepo.R
 
 class SettingsActivity : AppCompatActivity() {
@@ -16,7 +18,24 @@ class SettingsActivity : AppCompatActivity() {
                 .replace(R.id.settings, SettingsFragment())
                 .commit()
         }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        /* ToolBar */
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeButtonEnabled(true)
+            title = getString(R.string.settings)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
