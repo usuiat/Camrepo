6a13e78a6745c8785d6fb878bda953deda73b2e5
usuiat <stripewani@gmail.com>
2020-11-15 14:19:39 +0900

    Toolbarを配置して戻るボタンを表示。画面の回転を許可。

--- app/src/main/java/net/engawapg/app/camrepo/slideshow/SlideshowActivity.kt
+++ app/src/main/java/net/engawapg/app/camrepo/slideshow/SlideshowActivity.kt
@@ -1,13 +1,33 @@
 package net.engawapg.app.camrepo.slideshow
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_page.*
 import net.engawapg.app.camrepo.R
 
 class SlideshowActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_slideshow)
+
+        /* ToolBar */
+        setSupportActionBar(toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeButtonEnabled(true)
+            it.title = ""
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     companion object {
