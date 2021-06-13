81c5124c37d48b6efbb0d6d4aff2034d0deddc0a
usuiat <stripewani@gmail.com>
2021-06-13 19:37:25 +0900

    SettingsActivityを整理。

--- app/src/main/java/net/engawapg/app/camrepo/settings/SettingsActivity.kt
+++ app/src/main/java/net/engawapg/app/camrepo/settings/SettingsActivity.kt
@@ -1,44 +1,21 @@
 package net.engawapg.app.camrepo.settings
 
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import net.engawapg.app.camrepo.R
-import net.engawapg.app.camrepo.databinding.SettingsActivityBinding
 
 class SettingsActivity : AppCompatActivity() {
 
-    private lateinit var binding: SettingsActivityBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = SettingsActivityBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.settings_activity)
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
+                .replace(R.id.container, SettingsFragment())
                 .commit()
-        }
-
-        /* ToolBar */
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeButtonEnabled(true)
-            title = getString(R.string.settings)
-        }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                finish()
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
         }
     }
 
@@ -54,9 +31,5 @@ class SettingsActivity : AppCompatActivity() {
             }
 
         }
-    }
-
-    companion object {
-//        private const val TAG = "SettingsActivity"
     }
 }
